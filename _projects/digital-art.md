598d5411b2265a70bfeb0604d4228f6f66ed98f6
Sam Spilsbury <smspillaz@gmail.com>
2016-04-03 23:06:05 +0800

    digital-art: Add content

--- _projects/digital-art.md
+++ _projects/digital-art.md
@@ -5,5 +5,38 @@ thumb: /img/digital-art.svg
 blurb: New visions, new landscapes
 ---
 
-Compiz
-======
+Digital Art
+===========
+
+I like to make stuff that looks and feels compelling. Something that makes one
+sit back, laugh, and say "wow".
+
+To this end, exploring motion in artwork is somewhat of a passion of mine. I
+like to think about where things could go and how different landscapes might
+look from different distances.
+
+The below projects actually run live in the browser. They're written in
+C++ and compiled to JavaScript using a library called [Emscripten](http://github.com/kripken/emscripten).
+The rendering is done by an excellent library called [Magnum](http://github.com/mosra/magnum),
+which supports Emscripten natively.
+
+Floor Music Visualiser
+----------------------
+
+<div class="emscripten-wrapper">
+    <canvas class="emscripten-canvas" id="module"></canvas>
+    <div id="status" class="emscripten-status-description">Initialization...</div>
+    <script src="{{ "/js/Loader.js" | prepend: site.baseurl }}"></script>
+    <script async="async" src="{{ "/js/fmv-emscripten-wrapper.js" | prepend: site.baseurl }} "></script>
+</div>
+
+The idea behind this music visualiser was to think of the "dance floor"
+in Dance Dance Revolution. I was thinking of something that was vibrant and
+sleek, with clear outlines for each of the tiles. The idea expanded as I
+added motion to the floor as something that reacted dynamically to what was
+playing, almost like a cloth in the wind.
+
+The outlines on the bars are achieved by providing barymetric co-ordinates
+to the vertex shader. Pixels that are close to edges of the barymetric
+outline are colored with a solid color, other pixels are semitransparent
+based on the height of the bar.
---
+++ js/Loader.js
@@ -0,0 +1,153 @@
+function audio() {
+    var context = new (window.webkitAudioContext || window.AudioContext)();
+    var source;
+    var processor;
+    var analyser;
+    var xhr;
+    var disconnectTimeout = 0;
+
+    function initAudio(data) {
+        source = context.createBufferSource();
+        
+        if (context.decodeAudioData) {
+            context.decodeAudioData(data, function(buffer) {
+                source.buffer = buffer;
+                createAudio();
+            }, function(e) {
+                console.log(e);
+            });
+        } else {
+            source.buffer = context.createBuffer(data, false /*mixToMono*/);
+            createAudio();
+        }
+    }
+
+    function createAudio() {
+        processor = context.createJavaScriptNode(512 /*bufferSize*/,
+                                                 1 /*num inputs*/,
+                                                 1 /*num outputs*/);
+        processor.onaudioprocess = processAudio;
+
+        analyser = context.createAnalyser();
+            
+        source.connect(context.destination);
+        source.connect(analyser);
+
+        analyser.connect(processor);
+        processor.connect(context.destination);
+
+        source.noteOn(0);
+        disconnectTimeout = setTimeout(disconnect,
+                                       source.buffer.duration * 1000 +1000);
+    }
+
+    function disconnect() {
+        source.noteOff(0);
+        source.disconnect(0);
+        processor.disconnect(0);
+        analyser.disconnect(0);
+
+        disconnectTimeout = 0;
+    }
+
+    function processAudio(e) {
+        var freqByteData = new Float32Array(analyser.frequencyBinCount);
+        analyser.getFloatFrequencyData(freqByteData);
+        
+        var buf = Module._malloc(freqByteData.length * freqByteData.BYTES_PER_ELEMENT);
+        Module.HEAPF32.set(freqByteData, buf >> 2);
+        Module._set_frequencies(buf, analyser.minDecibels, analyser.maxDecibels - analyser.minDecibels);
+        Module._free(buf);
+    }
+
+    function dropEvent(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        if (disconnectTimeout) {
+            disconnect();
+            clearTimeout(disconnectTimeout);
+            disconnectTimeout = 0;
+        }
+
+        var droppedFiles = evt.dataTransfer.files;
+        
+        var reader = new FileReader();
+        
+        reader.onload = function(fileEvent) {
+            var data = fileEvent.target.result;
+            initAudio(data);
+        };
+
+        Module.setStatus("Now playing: " + droppedFiles[0].name);
+        
+        reader.readAsArrayBuffer(droppedFiles[0]);
+    }
+
+    function handleResult() {
+        if (xhr.readyState == 4 /* complete */) {
+            switch(xhr.status) {
+                case 200: /* Success */
+                    initAudio(request.response);
+                    break;
+                default:
+                    break;
+            }
+            xhr = null;
+        }
+    }
+
+    function dragOver(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        return false;
+    }
+
+    var dropArea = document.getElementById('module');
+    dropArea.addEventListener('drop', dropEvent, false);
+    dropArea.addEventListener('dragover', dragOver, false);
+
+}
+
+
+var Module = {
+    preRun: [],
+    postRun: [],
+    onRuntimeInitialized: function() {
+        audio();
+        Module._run_fmv_with_frequency_provider(document.body.clientWidth, document.body.clientHeight);
+    },
+
+    printErr: function(message) {
+        console.error(Array.prototype.slice.call(arguments).join(' '));
+    },
+
+    print: function(message) {
+        console.log(Array.prototype.slice.call(arguments).join(' '));
+    },
+
+    canvas: document.getElementById('module'),
+
+    setStatus: function(message) {
+        if (!message.length)
+            message = "Drag some music into this box to preview the player";
+
+        var status = document.getElementById('status');
+        if(status) status.innerHTML = message;
+    },
+
+    totalDependencies: 0,
+
+    monitorRunDependencies: function(left) {
+        this.totalDependencies = Math.max(this.totalDependencies, left);
+
+        if(left) {
+            Module.setStatus('Downloading... ' + (this.totalDependencies - left) + '/' + this.totalDependencies);
+        } else {
+            Module.setStatus('Download complete');
+        }
+    }
+};
+
+Module.setStatus('Downloading...');
+
