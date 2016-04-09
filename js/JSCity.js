90224073d75a7510327ce496e9a01efafb07661e
Sam Spilsbury <smspillaz@gmail.com>
2016-04-09 22:42:49 +0800

    JSCity: Add a bit more detail, lights etc

--- js/JSCity.js
+++ js/JSCity.js
@@ -1,3 +1,25 @@
+function generateTexture() {
+    var windowsCanvas = document.createElement("canvas");
+    windowsCanvas.width = 64;
+    windowsCanvas.height = 128;
+    var windowsContext = windowsCanvas.getContext("2d");
+    windowsContext.fillStyle = "#333344";
+    windowsContext.fillRect(0, 0, 64, 128);
+
+    for (var y = 2; y < 128; ++y) {
+        for (var x = 0; x < 8; ++x) {
+            if (Math.random() > 0.7) {
+                windowsContext.fillStyle = "#999955";
+                windowsContext.fillRect(x * 8, y, 8, 1);
+            }
+        }
+    }
+
+    return windowsCanvas;
+}
+
+
+
 var element = document.getElementById("js-city-canvas");
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 45, element.clientWidth / element.clientHeight, 0.1, 1000 );
@@ -9,21 +31,66 @@ element.appendChild( renderer.domElement );
 
 var start_x = -30;
 
+function generateRectangularBuilding(textureArray, i, j) {
+    var sceneArray = [];
+    var materialArray = [
+        new THREE.MeshLambertMaterial({ map: textureArray[0] }),
+        new THREE.MeshLambertMaterial({ map: textureArray[1] }),
+        new THREE.MeshLambertMaterial({color: 0x333344}),
+        new THREE.MeshLambertMaterial({ map: textureArray[2] }),
+        new THREE.MeshLambertMaterial({ map: textureArray[3] }),
+        new THREE.MeshLambertMaterial({color: 0x333344})
+    ];
+
+    var tower = new THREE.Mesh( new THREE.BoxGeometry( 1, Math.max(3, 2 + 4 * Math.random()), 1.0 ),
+                                new THREE.MeshFaceMaterial(materialArray));
+    tower.position.x = start_x + i * 2;
+    tower.position.z = -j * 2;
+    sceneArray.push( tower );
+
+    if (Math.random () > 0.7) {
+        var base = new THREE.Mesh (new THREE.BoxGeometry (1.5, Math.max(1, 0.5 + 2 * Math.random()), 1.5),
+                                   new THREE.MeshFaceMaterial(materialArray));
+        base.position.x = start_x + i * 2;
+        base.position.z = -j * 4;
+        sceneArray.push(base);
+    }
+
+    return sceneArray;
+}
+
+function generateSphericalBuilding(texture, i, j) {
+    var tower = new THREE.Mesh (new THREE.CylinderGeometry(1, 1, Math.max(1, 0.5 + 2 * Math.random())),
+                                new THREE.MeshLambertMaterial({map: texture}));
+    tower.position.x = start_x + i * 2;
+    tower.position.z = -j * 2;
+
+    return tower;
+}
+
+function generateTextureArray(n) {
+    var array = new Array(4);
+    for (var i = 0; i < array.length; ++i) {
+        var texture = new THREE.Texture(generateTexture());
+        texture.anisotropy  = renderer.getMaxAnisotropy();
+        texture.magFilter = THREE.NearestFilter;
+        texture.needsUpdate = true;
+        array[i] = texture;
+    }
+    return array;
+}
+
 for (var j = 0; j < 10; ++j) {
     for (var i = 0; i < 30; ++i) {
-        var tower = new THREE.Mesh( new THREE.BoxGeometry( 1, Math.max(3, 2 + 4 * Math.random()), 1.0 ),
-                                   new THREE.MeshLambertMaterial( { color: 0x333344 } ) );
-        tower.position.x = start_x + i * 2;
-        tower.position.z = -j * 2;
-        scene.add( tower );
-
-        if (Math.random () > 0.7) {
-            var base = new THREE.Mesh (new THREE.BoxGeometry (1.5, Math.max(1, 0.5 + 2 * Math.random()), 1.5),
-                                       new THREE.MeshLambertMaterial( { color: 0x333344 } ) );
-            base.position.x = start_x + i * 2;
-            base.position.z = -j * 4;
-            scene.add(base);
+        var val = Math.random();
+        if (val > 0.6) {
+            scene.add(generateSphericalBuilding(generateTextureArray(1)[0], i, j));
+        } else {
+            generateRectangularBuilding(generateTextureArray(4), i, j).map(function(obj) {
+                scene.add(obj);
+            });
         }
+        
     }
 }
 
@@ -50,7 +117,7 @@ var skyboxMaterialCube = new THREE.MeshFaceMaterial( materialArray );
 var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 skybox.position.y += 5;
 
-var light = new THREE.PointLight(0xffffff);
+var light = new THREE.PointLight(0x555577);
 light.position.set(0, 5, 5);
 
 scene.add(skybox);
